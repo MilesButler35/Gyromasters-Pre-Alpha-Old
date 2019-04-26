f2ba6b65344a5f4e812bb493952412ad65f7b0ea
MilesButler35 <mbutle35@uncc.edu>
2019-04-25 22:53:51 -0400

    Stage Select Up and Running
    Needs to be made prettier and need to set it so that it goes to the win screen on win.

---
+++ Top_Game_Prototype/Assets/Scripts/StageSelectManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageSelectManager : MonoBehaviour
+{
+
+    public void StageOneStart()
+    {
+        MySceneManager.GetInstance().RequestLevelLoad(SceneType.prog, "Stage 1");
+    }
+
+    public void StageTwoStart()
+    {
+        MySceneManager.GetInstance().RequestLevelLoad(SceneType.prog, "Stage 2");
+    }
+
+    public void StageThreeStart()
+    {
+        MySceneManager.GetInstance().RequestLevelLoad(SceneType.prog, "Stage 3");
+    }
+
+    public void BackButton()
+    {
+        MySceneManager.GetInstance().RequestLevelLoad(SceneType.prog, "TitleMenu");
+    }
+}
